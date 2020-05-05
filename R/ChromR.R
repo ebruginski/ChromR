6387537e5cc78f995ab870abe9fb20c69de7717a
Estevan Bruginski <estevan.bruginski@gmail.com>
2020-05-04 20:11:23 -0400

    add description

--- R/ChromR.R
+++ R/ChromR.R
@@ -1,7 +1,10 @@
 #' ChromR package
 #'
-#'
-#' This package contains functions to help chromatographers in the diary tasks.
+#' The ChromR package brings some functions to help analysts, Ph.D. students and technicians to plan and organize LC analyses.
+#' The functions are separated into 2 main sections, Pre-analyse and Post-analyse. In Pre-analyse, the functions will help to randomize
+#' the injections order,  calculation of the quantity of mobile phase and the spent time of batches.
+#' The Post-analyse functions will help to organize the obtained files according to their respective batches or classes,
+#' avoiding human error.
 #'
 #' @docType package
 #'
