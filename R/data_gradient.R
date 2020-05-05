aff3827b26f20a7395fc73881136a0b838e82414
Estevan Bruginski <estevan.bruginski@gmail.com>
2020-05-04 20:11:35 -0400

    new data example

---
+++ R/data_gradient.R
@@ -0,0 +1,13 @@
+#' @title Gradient profile method of LC.
+#'
+#' @description Example of gradient profile for LC system.
+#'
+#'
+#' @examples
+#' \dontrun{
+#'  gradient
+#' }
+#' @author Estevan Bruginski \email{estevan.bruginski@ufpr.br}
+#' Universidade Federal do Paraná
+#' License: MIT + file LICENSE
+"gradient"
---
+++ R/data_metadata.R
@@ -0,0 +1,20 @@
+#' @title Metadata example.
+#'
+#' @description Example of metadata for FileOrganizer.
+#'
+#' @format A data frame with 6 rows and 4 variables:
+#' \describe{
+#' \item{Samples}{Sample names.}
+#' \item{Class}{Class identification.}
+#' \item{Order}{Injection order.}
+#' \item{Batch}{Batch identification.}
+#' }
+#'
+#' @examples
+#' \dontrun{
+#'  metadata_ex
+#' }
+#' @author Estevan Bruginski \email{estevan.bruginski@ufpr.br}
+#' Universidade Federal do Paraná
+#' License: MIT + file LICENSE
+"metadata_ex"
