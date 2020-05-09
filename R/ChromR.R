0dba535cf5966f934b66fb40669ed6873a21d55f
Estevan Bruginski <estevan.bruginski@gmail.com>
2020-05-09 18:59:57 -0400

    Example img

--- R/ChromR.R
+++ R/ChromR.R
@@ -1,10 +1,11 @@
 #' ChromR package
 #'
-#' The ChromR package brings some functions to help analysts, Ph.D. students and technicians to plan and organize LC analyses.
-#' The functions are separated into 2 main sections, Pre-analyse and Post-analyse. In Pre-analyse, the functions will help to randomize
-#' the injections order,  calculation of the quantity of mobile phase and the spent time of batches.
-#' The Post-analyse functions will help to organize the obtained files according to their respective batches or classes,
-#' avoiding human error.
+#' The ChromR package brings some functions to help analysts, Ph.D. students
+#' and technicians to plan and organize LC analyses. The functions are separated into 2 main sections,
+#' Pre-analysis and Post-analysis. In Pre-analyse, the functions will help to randomize the injections
+#' order,  calculation of the quantity of mobile phase and the spent time of batches.
+#' The Post-analyse functions will help to organize the obtained files according to their respective
+#' batches or classes, avoiding human error.
 #'
 #' @docType package
 #'
