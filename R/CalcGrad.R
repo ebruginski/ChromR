1bbb179144d44e53178d159edb0ced5bc08c4adf
Estevan Bruginski <estevan.bruginski@gmail.com>
2023-01-29 22:39:20 -0300

    Atualização do RoxygenNote

--- R/CalcGrad.R
+++ R/CalcGrad.R
@@ -1,108 +1,96 @@
-#' Gradient mobile phase calculation
+#' Cálculo do volume de fase móvel e tempo total de um batch cromatográfico
 #'
-#' @description Calculate the volume of A and B mobile phases on a LC gradient.
+#' @description Cálculo do volume de fase móvel e tempo total de um batch cromatográfico.
 #'
-#' @param x Datapath character, gradient profile in .csv format
-#' @param runs Numeric, number of chromatographic runs in the batch.
-#' @param over Numeric, overage of mobile phase to keep the system secure. The default value is 15\%. Need to be set according with your system.
-#' @param plot Logical, plot the gradient profile, TRUE by default.
-#' @return a list with the total batch time and the amount, in ml, of solvents A and B.
+#' @param x Datapath character, arquivo em .csv do gradiente.
+#' @param runs Numeric, número de corridas no batch.
+#' @param over Numeric, limite de segurança para o sistema cromatográfico não secar. Valor padrão é 15%.
+#' @return uma lista com o volume total de cada fase móvel em mL (A e B) e tempo total
 #'
 #' @examples
 #' \dontrun{
-#' GradCalc(x, runs = 15, over = 15, plot = TRUE)
+#' GradCalc(x, runs = 15, over = 15)
 #'}
-#' @importFrom utils read.csv
-#' @importFrom utils tail
-#' @importFrom lubridate seconds_to_period
+#' @import lubridate
+#' @import dplyr
+#' @import data.table
 #'
 #' @export
 #' @author Estevan Bruginski \email{estevan.bruginski@ufpr.br}
 #' Universidade Federal do Paraná
 #' License: MIT + file LICENSE
 #'
-CalcGrad <- function(x, runs, over = 15, plot = TRUE) {
-  ## Import the gradient profile
+CalcGrad <- function(x, runs, over = 15) {
 
-  gradprof <- read.csv(x, header = TRUE, check.names = FALSE)
+  ## importa o gradiente
+  gradprof <-
+    fread(x,
+          header = TRUE,
+          check.names = FALSE)
 
-  test.comp.t <- (gradprof$A + gradprof$B) == 100
+  ## valida a soma das fases A e B é 100
+  val.grad <- gradprof %>%
+    filter((A + B) == 100) %>%
+    nrow() == nrow(gradprof)
 
-  test.time <- gradprof$Time
-  ## Check the initial time = 0
-  if (gradprof$Time[1] >= 1) {
-    message("The initial time need to be zero!")
+  ## valida o tempo inicial como 0
+  val.intime <- gradprof %>%
+    filter(row_number() == 1) %>%
+    filter(Time == 0) %>%
+    nrow() > 0
 
-    ## Check the increasing time
-  } else if (all(diff(test.time) > 0) == FALSE) {
-    message("The time of gradient need to increase!")
+  ## valida o aumento do tempo
+  val.time <- gradprof %>%
+    mutate(maior = Time > lag(Time)) %>%
+    select(maior) %>%
+    filter(row_number() != 1) %>%
+    all()
 
-    ## Build the checked gradient profile
-  } else if (sum(test.comp.t) == length(test.comp.t)) {
+  ## Cálcula o o volume de fases móveis e o tempo total do batch cromatográfico
+  if (val.grad == TRUE &
+      val.intime == TRUE &
+      val.time == TRUE) {
 
-    time.grad <- as.numeric(gradprof$Time)
+    gradient <- gradprof %>%
+      select(Time, Flow, A, B) %>%
+      mutate(dt = Time - lag(Time))
 
-    flow.grad <- as.numeric(gradprof$Flow)
+    steps.A <- gradient %>%
+      mutate(vol = Flow * dt * (A + lag(A)) / 200) %>%
+      pull(vol)
 
-    comp.A <- as.numeric(gradprof$A)
+    steps.B <- gradient %>%
+      mutate(vol = Flow * dt * (B + lag(B)) / 200) %>%
+      pull(vol)
 
-    comp.B <- as.numeric(gradprof$B)
+    total.A <- sum(steps.A, na.rm = TRUE) * runs
 
-    gradprof_check <- data.frame(time.grad, flow.grad, comp.A, comp.B);
+    total.B <- sum(steps.B, na.rm = TRUE) * runs
 
-    ## Empty vectors to store the vol of each gradient step
-    steps.A <- NULL
-    steps.B <- NULL
+    batch.time <- gradprof %>%
+      slice_tail(n = 1) %>%
+      mutate(batch.time = Time * 60 * runs) %>%
+      mutate(batch.time = lubridate::seconds_to_period(batch.time)) %>%
+      select(batch.time)
 
-    ## Compute the volume for each gradient step
-    for (i in 1:nrow(gradprof_check)) {
-      steps.A[i] <-
-        gradprof_check[i, 2] * (gradprof_check[i + 1, 1] - gradprof_check[i, 1]) * (gradprof_check[i, 3] + gradprof_check[i + 1, 3]) / 200
-      steps.B[i] <-
-        gradprof_check[i, 2] * (gradprof_check[i + 1, 1] - gradprof_check[i, 1]) * (gradprof_check[i, 4] + gradprof_check[i + 1, 4]) / 200
-    }
-
-    ## Sum the the volume of each phase and multiply by the number of runs to get the total vol A and B
-    total.A.tmp <- sum(steps.A, na.rm = TRUE) * runs
-    total.B.tmp <- sum(steps.B, na.rm = TRUE) * runs
-
-    #mobile phase overage to secure the LC system
-    over <- over / 100
-
-    total.A <- round(total.A.tmp + (total.A.tmp * over), 1)
-    total.B <- round(total.B.tmp + (total.B.tmp * over), 1)
-
-    ## Calculate the total batch time
-    run.time <- tail(gradprof_check, 1)
-
-    run.time <- as.numeric(run.time[1]) * 60
-
-    batch.time <- run.time * runs
-
-    batch.time <- lubridate::seconds_to_period(batch.time)
-
-    ## List of results
     result <- list(total.A, total.B, batch.time)
 
     names(result) <- c("solvent_A", "solvent_B", "total_time")
 
-    result_t <- paste("Total volume of phase A:", result$solvent_A,"ml. Total volume of phase B:", result$solvent_B,"ml. The total time will be", result$total_time,"." )
+    result_t <-
+      paste(
+        "Volume total de fase A:",
+        result$solvent_A,
+        "ml. Volume total de fase B:",
+        result$solvent_B,
+        "ml. O tempo total será de:",
+        result[["total_time"]][["batch.time"]],
+        "."
+      )
 
-    if (plot == TRUE){
-
-      pgrad <- PlotGrad(gradprof_check);
-      plot(pgrad)
-      return(print(result_t))
-
-    } else {
-
-      return(print(result_t))
-    }
-
+    return(print(result_t))
 
   } else{
-    message("The compositions A and B can't be more than 100%.")
-
+    message("Verifique o seu gradiente!")
   }
-}
-
+  }
